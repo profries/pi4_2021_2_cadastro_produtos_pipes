0aa6f317c9e9869a98806bbeef98cb70324a1788
Luis Henrique Ries <prof.ries@gmail.com>
2021-11-04 22:03:34 -0300

    Adicionado o pipe Moeda para formatacao da moeda corretamente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { TabelaProdutosComponent } from './tabela-produtos/tabela-produtos.component';
 import { FormProdutosComponent } from './form-produtos/form-produtos.component';
 import { FormsModule } from '@angular/forms';
+import { MoedaPipe } from './moeda.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabelaProdutosComponent,
-    FormProdutosComponent
+    FormProdutosComponent,
+    MoedaPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/moeda.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'moeda'
+})
+export class MoedaPipe implements PipeTransform {
+  transform(value: number): string {
+    const valor = value.toFixed(2);
+    const moeda = 'R$ ' + valor.replace('.', ',');
+    return moeda;
+  }
+
+}
